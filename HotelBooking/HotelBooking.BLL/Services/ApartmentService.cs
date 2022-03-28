d51db3a418af45d18eaba6b2423c273406df4eb8
KiriIl <375447360162@mail.ru>
2022-03-28 21:40:01 +0300

    add apartment service

---
+++ HotelBooking/HotelBooking.BLL/Services/ApartmentService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HotelBooking.BLL.DTOModels;
+using HotelBooking.BLL.Services.IServices;
+using HotelBooking.DAL.DataModels;
+using HotelBooking.DAL.Models;
+using HotelBooking.DAL.Repositories.IRepositories;
+
+namespace HotelBooking.BLL.Services
+{
+    public class ApartmentService : IApartmentService
+    {
+        private IMapper _mapper;
+        private IApartmentRepository _apartmentRepository;
+
+        public ApartmentService(
+            IMapper mapper,
+            IApartmentRepository apartmentRepository)
+        {
+            _mapper = mapper;
+            _apartmentRepository = apartmentRepository;
+        }
+
+        public void CreateApartment(ApartmentDTO apartment)
+        {
+            var apartmentDM = _mapper.Map<ApartmentDataModel>(apartment);
+            _apartmentRepository.Save(_mapper.Map<Apartment>(apartmentDM));
+        }
+    }
+}
