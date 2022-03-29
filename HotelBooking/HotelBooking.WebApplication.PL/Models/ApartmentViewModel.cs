882783a83894e427a4793673ec71ade5aab375cd
KiriIl <375447360162@mail.ru>
2022-03-29 14:16:51 +0300

    added validation attr

--- HotelBooking/HotelBooking.WebApplication.PL/Models/ApartmentViewModel.cs
+++ HotelBooking/HotelBooking.WebApplication.PL/Models/ApartmentViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using HotelBooking.Common.Enums;
+using HotelBooking.WebApplication.PL.Models.ValidationAttributes;
 
 namespace HotelBooking.WebApplication.PL.Models
 {
     public class ApartmentViewModel
     {
+        [MinValue(1)]
         public int RoomsCount { get; set; }
+        [MinValue(0)]
         public decimal Cost { get; set; }
         public RoomType RoomType { get; set; }
         public string Info { get; set; }
