f5ce5b464672d4b8cab65d6f8796be04c56b3708
KiriIl <375447360162@mail.ru>
2022-03-31 14:41:23 +0300

    added action and booking view model

--- HotelBooking/HotelBooking.WebApplication.PL/Controllers/HomeController.cs
+++ HotelBooking/HotelBooking.WebApplication.PL/Controllers/HomeController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HotelBooking.WebApplication.PL.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBooking.WebApplication.PL.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
+        [AllowAnonymous]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult BookingApartment(long id)
+        {
+            var viewModel = new BookingApartmentViewModel() { IdApartment = id };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult BookingApartment(BookingApartmentViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //find current user
+            }
+
             return View();
         }
     }
---
+++ HotelBooking/HotelBooking.WebApplication.PL/Models/BookingApartmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBooking.WebApplication.PL.Models
+{
+    public class BookingApartmentViewModel
+    {
+        //public ApartmentViewModel {get; set;}
+        public long IdApartment { get; set; }
+        public long IdUser { get; set; }
+        [Required]
+        public DateTime ArrivalDate { get; set; }
+        [Required]
+        public DateTime DepartureDate { get; set; }
+    }
+}
